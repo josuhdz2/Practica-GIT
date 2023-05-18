7ac7a491f2f76a16c2bb1c74ec5d15b5b6a849d7
josuhdz2 <josueherdeveloper@gmail.com>
2023-05-17 18:13:58 -0600

    Finalizando con la funcionalidad de login y bloqueo de rutas

--- home.js
+++ home.js
@@ -2,3 +2,11 @@ if(localStorage.getItem('login')!="success")
 {
     window.location.href="index.html";
 }
+document.getElementById('logout').addEventListener('click', ()=>
+{
+    if(confirm('Seguro que deseas cerrar sesion?'))
+    {
+        localStorage.clear();
+        window.location.reload();
+    }
+})
